ace77c565f12c00081465b667184b3a044b704e1
Caio Ferreira <ca.victor2@gmail.com>
2024-12-26 00:40:46 +0000

    :recycle: refactor Deixando o `database.js` `client.connect()` mais robusto ao ldar com erros

--- infra/database.js
+++ infra/database.js
@@ -8,19 +8,25 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
-  await client.connect();
+  console.log("Credenciais do POSTGRES", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   let result = {};
 
   try {
+    await client.connect();
     result = await client.query(queryObject);
+    return result;
   } catch (err) {
     console.error(err);
+    throw err;
   } finally {
     await client.end();
   }
-
-  return result;
 }
 
 export default {
