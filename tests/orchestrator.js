4ad176488581ac61ee7e0fef46b786bfd77dc025
Caio Ferreira <ca.victor2@gmail.com>
2025-01-12 01:43:19 +0000

    :ok_hand: style fix `prettier` linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -8,21 +8,22 @@ async function waitForAllServices() {
       retries: 100,
       maxTimeout: 1000,
       onRetry: (error, attempt) => {
-        console.log(`Tentativa ${attempt} - Falha no fetch com status: ${error.message}`);
-      }
+        console.log(
+          `Tentativa ${attempt} - Falha no fetch com status: ${error.message}`,
+        );
+      },
     });
 
     async function fetchStatusPage() {
       try {
         const response = await fetch("http://localhost:3000/api/v1/status");
-        if(response.status !== 200){
+        if (response.status !== 200) {
           throw new Error(`HTTP error ${response.status}`);
         }
         await response.json();
       } catch (error) {
         throw error;
       }
-      
     }
   }
 }
