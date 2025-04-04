72be52dd797fd882b1ef8b1c75f19e18123090f0
Caio Ferreira <ca.victor2@gmail.com>
2025-04-04 19:44:59 +0000

    refactor: :recycle: use `migrator` model in `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,5 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
+import migrator from "models/migrator";
 import controller from "infra/controller.js";
 
 const router = createRouter();
@@ -11,42 +9,17 @@ router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-async function getDefaultMigrationsOptions() {
-  const dbClient = await database.getNewClient();
-
-  return {
-    dbClient: dbClient,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-}
-
 async function getHandler(request, response) {
-  const defaultMigrations = await getDefaultMigrationsOptions();
-  return migrationRunner({
-    ...defaultMigrations,
-    dryRun: true,
-  })
-    .then((pendingMigrations) => {
-      return response.status(200).json(pendingMigrations);
-    })
-    .finally(() => {
-      defaultMigrations?.dbClient.end();
-    });
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  const defaultMigrations = await getDefaultMigrationsOptions();
-  return migrationRunner(defaultMigrations)
-    .then((migratedMigrations) => {
-      if (migratedMigrations?.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
-      return response.status(200).json(migratedMigrations);
-    })
-    .finally(() => {
-      defaultMigrations?.dbClient.end();
-    });
+  const migratedMigrations = await migrator.runPedingMigrations();
+
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
+  }
+
+  return response.status(200).json(migratedMigrations);
 }
